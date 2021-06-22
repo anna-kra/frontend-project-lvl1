ef004f3f9d4f40168ade86889bfc5a36b89f177d
Anna <info2@akrymedia.com>
2021-06-23 01:06:53 +0200

    The first game (brain-games) is ready

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import { getText, question } from '../src/cli';
+import { question } from '../src/cli.js';
 
-console.log(getText('Welcome to the Brain Games!'));
+console.log('Welcome to the Brain Games!');
 const userName = question('May I have your name? ');
 console.log(`Hello, ${userName}!`);
--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,4 @@
 import readlineSync from 'readline-sync';
 
-export const getText = (text) => text;
 export const question = (text) => readlineSync.question(text);
+export const getText = (text) => text;
