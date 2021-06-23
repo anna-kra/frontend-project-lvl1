8605715fd0e725003932738bdc7020207327a344
Anna <info2@akrymedia.com>
2021-06-23 18:40:05 +0200

    The game brain-even is complete

--- src/brain-even-functions.js
+++ src/brain-even-functions.js
@@ -1,26 +1,40 @@
 import { question, greeting } from './cli.js';
 
 const getRandomInt = (max) => Math.floor(Math.random() * max);
-
 const isPrime = (num) => ((num % 2 === 0) ? 'yes' : 'no');
 
-export const proofAnswerCorrect = (randomInt, answer, userName) => {
+export const isAnswerCorrect = (randomInt, answer, userName) => {
   let output = '';
+
   if (isPrime(randomInt) === answer) {
     output = 'Correct!';
   } else {
     output = `'${answer}' is wrong answer ;(. Correct answer was '${isPrime(randomInt)}'. Let's try again, ${userName}!`;
   }
+
   return output;
 };
 
 export const brainEven = () => {
   const userName = greeting();
+  let counter = 0;
 
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  const randomInt = getRandomInt(1000);
+  for (let i = 1; i <= 3; i += 1) {
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+    const randomInt = getRandomInt(1000);
 
-  console.log(`Question: ${randomInt}`);
-  const answer = question('Your answer: ');
-  console.log(proofAnswerCorrect(randomInt, answer, userName));
+    console.log(`Question: ${randomInt}`);
+    const answer = question('Your answer: ');
+    const answerCorrect = isAnswerCorrect(randomInt, answer, userName);
+    console.log(isAnswerCorrect(randomInt, answer, userName));
+
+    if (answerCorrect !== 'Correct!') {
+      break;
+    } else {
+      counter += 1;
+    }
+  }
+  if (counter === 3) {
+    console.log(`Congratulations, ${userName}`);
+  }
 };
