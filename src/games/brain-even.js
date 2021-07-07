bb62ee209e458166033f867ce96e5173f1587b93
Anna <info2@akrymedia.com>
2021-07-08 01:25:51 +0200

    Changed src/games/brain-even.js

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -10,9 +10,31 @@ const DIRECTIVE_EVEN = 'Answer "yes" if the number is even, otherwise answer "no
 
 const MAX_RANDOM_NUMBER = 1000;
 
-const isEven = (num) => ((num % 2 === 0) ? 'yes' : 'no');
+const GAME_ROUNDS = 3;
 
-const isAnswerCorrect = (randomInt, userAnswer) => isEven(randomInt) === userAnswer;
+// const isEven = (num) => ((num % 2 === 0) ? 'yes' : 'no');
+
+const isEven = (num) => (num % 2 === 0);
+
+// const isAnswerCorrect = (randomInt, userAnswer) => isEven(randomInt) === userAnswer;
+
+const isAnswerCorrect = (randomInt, userAnswer) => (
+  (isEven(randomInt) === true && userAnswer === 'yes') || (isEven(randomInt) === false && userAnswer === 'no')
+);
+
+// isAnswerCorrect(randomInt, userAnswer) ? 'yes' : 'no'
+
+const getAnswerCorrect = (randomInt) => {
+  let correctAnswer = '';
+
+  if (isEven(randomInt)) {
+    correctAnswer = 'yes';
+  } else if (!isEven(randomInt)) {
+    correctAnswer = 'no';
+  }
+
+  return correctAnswer;
+};
 
 const startBrainEven = () => {
   let counter = 0;
@@ -22,13 +44,15 @@ const startBrainEven = () => {
   printGreetingByName(userName);
   printDirective(DIRECTIVE_EVEN);
 
-  for (let i = 1; i <= 3; i += 1) {
+  for (let i = 1; i <= GAME_ROUNDS; i += 1) {
     const randomInt = getRandomInt(MAX_RANDOM_NUMBER);
-    const correctAnswer = isEven(randomInt);
+    // const correctAnswer = isEven(randomInt);
 
     printQuestion([randomInt]);
     const userAnswer = getUserAnswer();
+    const correctAnswer = getAnswerCorrect(randomInt, userAnswer);
 
+    // console.log(isAnswerCorrect(randomInt, userAnswer));
     if (isAnswerCorrect(randomInt, userAnswer)) {
       printCorrect();
       counter += 1;
