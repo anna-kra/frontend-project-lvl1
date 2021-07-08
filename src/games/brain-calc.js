c6f6f472e8d4de9ab546e4579851fcb28312485c
Anna <info2@akrymedia.com>
2021-07-08 09:28:54 +0200

    Refactoring src/games/brain-calc.js

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -12,7 +12,8 @@ const GAME_ROUNDS = 3;
 
 const getRandomSymbol = (arrSymbols) => arrSymbols[getRandomInt(arrSymbols.length - 1)];
 
-const calculate = (num1, num2, symbol) => {
+// const calculate = (num1, num2, symbol) => {
+const getCorrectAnswer = (num1, num2, symbol) => {
   let result = 0;
 
   switch (symbol) {
@@ -41,7 +42,8 @@ const startBrainCalc = () => {
     const randomInt1 = getRandomInt(MAX_RANDOM_NUMBER);
     const randomInt2 = getRandomInt(MAX_RANDOM_NUMBER);
     const randomSymbol = getRandomSymbol(['+', '-', '*']);
-    const correctAnswer = calculate(randomInt1, randomInt2, randomSymbol);
+    // const correctAnswer = calculate(randomInt1, randomInt2, randomSymbol);
+    const correctAnswer = getCorrectAnswer(randomInt1, randomInt2, randomSymbol);
 
     printQuestion([randomInt1, randomSymbol, randomInt2]);
     const userAnswer = Number(getUserAnswer());
