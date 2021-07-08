5db3ed2a332c75e5fed9f1aaac33029a8d8d764c
Anna <info2@akrymedia.com>
2021-07-08 12:57:16 +0200

    Changed bin/brain-games.js, Deleted src/games/brain-games copy.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import { startGame } from '../src/cli.js';
+import { greetUser } from '../src/cli.js';
 
-startGame();
+greetUser();
--- src/games/brain-games copy.js
+++
@@ -1,9 +0,0 @@
-import { getUserName, printGreeting, printGreetingByName } from '../cli.js';
-
-const startBrainGames = () => {
-  printGreeting();
-  const userName = getUserName();
-  printGreetingByName(userName);
-};
-
-export default startBrainGames;
