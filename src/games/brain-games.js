afe7ecf7788dc9a15eaf9aaa23a4ae108197dd51
Anna <info2@akrymedia.com>
2021-07-09 12:32:08 +0200

    Changed a call of the function greetUser in brain-games.js. Added parameters

--- src/games/brain-games.js
+++ src/games/brain-games.js
@@ -1,3 +1,7 @@
 import { greetUser } from '../cli.js';
 
-greetUser();
+const WELCOME_TEXT = 'Welcome to the Brain Games!';
+const ASC_FOR_NAME = 'May I have your name? ';
+const GREETING_WORD = 'Hello';
+
+greetUser(WELCOME_TEXT, ASC_FOR_NAME, GREETING_WORD);
