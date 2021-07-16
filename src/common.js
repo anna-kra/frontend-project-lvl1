93372a0b7329fb126cb5a00d3c73007fc81aeab5
Anna <info2@akrymedia.com>
2021-07-16 04:42:43 +0200

    Deletet a comment in common.js

--- src/common.js
+++ src/common.js
@@ -1,4 +1,3 @@
-// const getRandomInt = (max) => Math.floor(Math.random() * max) + 1;
 const getRandomInt = (min, max) => min + Math.floor(Math.random() * (max - min));
 
 export default getRandomInt;
