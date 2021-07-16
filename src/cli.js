419cf6fafa02419fbea04be698728296b1d6da20
Anna <info2@akrymedia.com>
2021-07-16 03:01:21 +0200

    Fixed a wrong import in bin/brain-games.js

--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,7 @@
 import readlineSync from 'readline-sync';
 
-// export const askQuestion = (text) => readlineSync.question(text);
-
-// export const printQuestion = (arrParam) => console.log(`Question: ${arrParam.join(' ')}`);
-
 const greetUser = () => {
   console.log('Welcome to the Brain Games!');
-  // const userName = askQuestion('May I have your name? ');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   return userName;
